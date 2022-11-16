cbc60ff4be6deb457d1b4bd966d6f931456c1c62
Keunoh <rmsdhcjswo1@gmail.com>
2022-11-16 23:24:14 +0900

    feat: member service

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,46 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MemberService {
+    //트랜잭셔널 기본으로 리드 온니하면
+    //그냥 읽어오는 값들을 좀더 최적화 성능으로 가져온다.
+    private final MemberRepository memberRepository;
+
+    /**
+     * 회원가입
+     */
+    //이게 좀더 스페시픽하므로 우선권가짐
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member); //중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    private void validateDuplicateMember(Member member) {
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    //회원 전체 조회
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
