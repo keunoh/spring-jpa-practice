79a85778a587136523001e882053515da9504cb3
Keunoh <rmsdhcjswo1@gmail.com>
2022-11-22 01:44:56 +0900

    fetch: the problem of returning entity direct

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
@@ -9,6 +11,14 @@ public class JpashopApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JpashopApplication.class, args);
+	}
+
+	//지연 로딩을 무시하라는 모듈
+	//쓰지 말라 (엔티티를 노출하지 말라는 뜻)
+	//해당 모듈의 필요한 기능은 써도 된다.
+	@Bean
+	Hibernate5Module hibernate5Module() {
+		return new Hibernate5Module();
 	}
 
 }
---
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * XToOne(ManyToOne, OneToOne)
+ * 성능 최적화
+ * Order
+ * Order -> Member 연관
+ * Order -> Delivery 연관
+ */
+@RestController
+@RequiredArgsConstructor
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName(); //LAZY 강제 초기화
+            order.getDelivery().getAddress(); //LAZY 강제 초기화
+        }
+
+        return all;
+    }
+
+}
